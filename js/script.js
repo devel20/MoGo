e2f50fb73559304219b5b6f42378c44eef22667b
Oleg <67776507+devel20@users.noreply.github.com>
2020-07-14 13:10:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,16 +11,12 @@ function open() {
 
 // accordeon ***********************************************
 
-let panelItem = document.querySelectorAll('.accordeon__header'),
-  active = document.getElementsByClassName('panel-active');
-
-  Array.from(panelItem).forEach(function(item, i, panelItem) {
-  item.addEventListener('click', function(e) {
-  if (active.length > 0 && active[0] !== this) // если есть активный элемент, и это не тот по которому кликнули
-  active[0].classList.remove('panel-active'); // убрать класс panel-active
-  // изменить состояние класса panel-active на текущем элементе: добавить если не было, убрать если было.
-  this.classList.toggle('panel-active');
+jQuery(document).ready(function($) {
+  $('.accordeon__header').on('click', function(event) {
+      $(this).toggleClass('active').next().slideToggle(300);
+      $('.accordeon__header').not(this).removeClass('active').next().slideUp(300);
   });
+
 });
 
 // carusel*************************************************
