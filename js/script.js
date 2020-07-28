0d50760ae1b8f37ae833936ee2fe3d75bfbbcf5b
Oleg <67776507+devel20@users.noreply.github.com>
2020-07-28 16:22:02 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,6 +11,25 @@ let link = $('a[href]');
   });
 });
 
+
+// плвный переход к якорю ***************************************
+$(document).ready(function(){
+$(".header__menu").on("click","a", function (event) {
+//отменяем стандартную обработку нажатия по ссылке
+event.preventDefault();
+
+//забираем идентификатор бока с атрибута href
+var id = $(this).attr('href'),
+
+//узнаем высоту от начала страницы до блока на который ссылается якорь
+top = $(id).offset().top;
+
+//анимируем переход на расстояние - top за 1000 мс
+$('body,html').animate({scrollTop: top}, 1000);
+});
+});
+
+
 // scroll Top ***************************************************
 jQuery(document).ready(function($) {
 $(window).scroll(function() {
